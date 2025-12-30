496fd5ce1fe9306d4094bed4120675366a69aae3
amish2005 <amishsharma2005@gmail.com>
2025-12-30 17:07:39 +0530

    fix: bundle server for vercel deployment

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { createServer } from '../server/index.js';
+import { createServer } from '../dist/server/production.mjs';
 import serverless from 'serverless-http';
 
 const app = createServer();
